e0f5bff0fbf057da84642919c2d7dd72ba2c0396
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-16 22:42:39 -0300

    Independencia Financeiura

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Red;
+for (int i = 0; i < 24; i++)
+{
+    Console.Write("Gastos mensais (em R$): "[i]);
+    Thread.Sleep(65);
+}
+decimal gastomensais = Convert.ToDecimal(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.Yellow;
+for (int i = 0; i < 25; i++)
+{
+    Console.Write("Rendimentos mensais (%): "[i]);
+    Thread.Sleep(65);
+}
+decimal rendimentomensais = Convert.ToDecimal(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"\nPara receber R$ {gastomensais:N2} por mês com rendimentos de {rendimentomensais:N1}% você precisa ter investido R$ {(gastomensais/(rendimentomensais/100)):N2}.");
+Console.ResetColor();
